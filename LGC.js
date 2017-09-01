36166906db9ae906f8d9d3f68711be2d14ee4303
Gregori <leon.gregori@accenture.com>
2017-09-01 21:53:31 +0200

    second commit

---
+++ LGC.js
@@ -0,0 +1,47 @@
+//Import SHA256 Crypto Algorithm
+const SHA256 = require('crypto-js/sha256');
+
+//This class represents a block. Variables init in the constructor
+class Block{
+    constructor(timestamp,data,prevHash = ''){
+        this.index = 0;
+        this.timestamp = timestamp;
+        this.data = data;
+        this.prevHash = prevHash;
+        this.hash = this.calculateHash();
+    }
+    
+//Calculates Hash based on Block variables
+    calculateHash(){
+        return SHA256(this.index + this.prevHash + this.timestamp + JSON.stringify(this.data)).toString();
+    }
+}
+
+//This class represents the blockchain including fucntions to get the latest block and add a new block. The genesis block is the first block on the chain and needs to manually added
+class Blockchain{
+    constructor(){
+        this.chain = [this.createGenesisBlock()];
+    }
+    
+    createGenesisBlock(){
+        return new Block("01/09/2017","Genesis block", "0")
+    }
+    
+    getLatestBlock(){
+        return this.chain[this.chain.length - 1];
+    }
+    
+    addBlock(newBlock){
+        newBlock.prevHash = this.getLatestBlock().hash;
+        newBlock.index = this.getLatestBlock().index + 1;
+        newBlock.hash=newBlock.calculateHash();
+        this.chain.push(newBlock);
+    }
+
+}
+
+let LeonCoin = new Blockchain();
+LeonCoin.addBlock(new Block("01/09/2017",{amount: 7}))
+LeonCoin.addBlock(new Block("02/09/2017",{amount: 2}))
+
+console.log(JSON.stringify(LeonCoin, null,4));
